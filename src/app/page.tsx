5dd6b8f4bbbab70c172e744b98e650064876a76d
mohammad <mohammdghiasi005@gmail.com>
2024-03-25 11:27:53 +0330

    complete-register-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,83 +2,160 @@
 import { useForm, Controller } from 'react-hook-form';
 import { TextField, Button, Container, Typography, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {Kanit} from "next/font/google"
-const font = Kanit({subsets: ["latin"], weight: "600"});
-const theme = createTheme();
+import { Kanit } from "next/font/google"
+const font = Kanit({ subsets: ["latin"], weight: "600" });
+const theme = createTheme({
+  typography: {
+    "fontFamily": `${font.className}`,
+  },
+});
 
 export default function RegisterPage() {
-  const { handleSubmit, control, reset, formState: { errors } } = useForm();
 
-  const onSubmit = (data: any) => {
+  const { register, control, handleSubmit, formState, reset } = useForm<form>()
+
+  const { errors, isSubmitting, isSubmitted, isSubmitSuccessful } = formState
+
+
+  const onSubmit = (data: form) => {
     // Simulate registration process
     console.log(data);
     // Reset the form after successful registration
-    reset();
+    reset()
   };
 
   return (
     <ThemeProvider theme={theme}>
-      <Container className="py-4 shadow-xl bg-inherit"  maxWidth="sm">
-        <Typography className={`${font.className}`} variant="h4" gutterBottom>
+      <Container className="py-4 shadow-2xl mt-3 shadow-gray-400 bg-inherit rounded-lg" maxWidth="sm">
+        <Typography variant="h4" gutterBottom>
           Register
         </Typography>
         <form onSubmit={handleSubmit(onSubmit)}>
           <FormControl fullWidth margin="normal">
-            <Controller
+            <TextField
+              {...register('name', {
+                pattern: {
+                  value: /^[a-zA-Z\s]+$/,
+                  message: 'Name must only contain letters and spaces',
+                },
+                required: {
+                  value: true,
+                  message: "username is required"
+                }
+              })}
+              required
+              type='text'
+              id='name'
+              color='warning'
+              label="Name"
+              error={errors.name?.message !== undefined}
+              helperText={errors.name?.message}
+            />
+          </FormControl>
+          {/* end of name ....... */}
 
-              name="name"
-              control={control}
-              defaultValue=""
-              rules={{ required: 'Name is required' }}
-              render={({ field }) => <TextField {...field} color='warning' label="Name" error={!!errors.name} helperText={errors.name && errors.name.message} />}
-            />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <Controller
 
-              name="email"
-              control={control}
-              defaultValue=""
-              rules={{ required: 'Email is required', pattern: { value: /^\S+@\S+$/i, message: 'Invalid email address' } }}
-              render={({ field }) => <TextField {...field} color='warning' label="Email" error={!!errors.email} helperText={errors.email && errors.email.message} />}
-            />
-          </FormControl>
-          <FormControl fullWidth margin="normal">
-            <Controller
 
-              name="password"
-              control={control}
-              defaultValue=""
-              rules={{ required: 'Password is required', minLength: { value: 8, message: 'Password must be at least 8 characters long' } }}
-              render={({ field }) => <TextField {...field} color='warning' type="password" label="Password" error={!!errors.password} helperText={errors.password && errors.password.message} />}
-            />
-          </FormControl>
           <FormControl fullWidth margin="normal">
-            <Controller
-              name="age"
-              control={control}
-              defaultValue=""
-              rules={{ required: 'Age is required', pattern: { value: /^[0-9]*$/, message: 'Invalid age' } }}
-              render={({ field }) => <TextField {...field} color='warning' type="number" label="Age" error={!!errors.age} helperText={errors.age && errors.age.message} />}
+            <TextField
+              {...register("email", {
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: 'Invalid email address'
+                },
+                required: {
+                  value: true,
+                  message: "Email is required"
+                }
+              })}
+              required
+              type='email'
+              id="email"
+              color='warning'
+              label="Email"
+              error={errors.email?.message !== undefined}
+              helperText={errors.email?.message}
             />
           </FormControl>
+          {/* end of email input */}
+
+
           <FormControl fullWidth margin="normal">
-            <Controller
-            
-              name="dateOfBirth"
-              control={control}
-              defaultValue=""
-              rules={{ required: 'Date of Birth is required' }}
-              render={({ field }) => <TextField {...field} color='warning' type="date" label="Date of Birth" error={!!errors.dateOfBirth} helperText={errors?.dateOfBirth && errors?.dateOfBirth.message} />}
+            <TextField
+              {...register("password", {
+                required: {
+                  value: true,
+                  message: "password is required"
+                },
+                minLength: {
+                  value: 8,
+                  message: 'Password must be at least 8 characters long'
+                }
+              })}
+              required
+              id="password"
+              color='warning'
+              type="password"
+              label="Password"
+              error={errors.password?.message !== undefined}
+              helperText={errors.password?.message}
             />
           </FormControl>
+          {/* end of passworf input */}
+
+
+          <FormControl fullWidth margin="normal">
+            <TextField
+              {...register("age", {
+                required: {
+                  value: true,
+                  message: "age in reqiured"
+                },
+                pattern: {
+                  value: /^[0-9]*$/,
+                  message: 'Invalid age input'
+                }
+              }
+              )}
+              required
+              id="age"
+              color='warning'
+              type="number"
+              label="Age"
+              error={errors.age?.message !== undefined}
+              helperText={errors.age?.message}
+            />
+          </FormControl>
+          {/* end age filed */}
+
+
+          <FormControl fullWidth margin="normal">
+            <TextField
+              {...register("dateOfBirth", {
+                required: {
+                  value: true,
+                  message: "date of birth is required"
+                }
+              })}
+              required
+              id="dateOfBirth"
+              color='warning'
+              type="date"
+              // value="mm/dd/yyyy"
+              label="Date of Birth"
+              error={!!errors.dateOfBirth}
+              helperText={errors.dateOfBirth && errors.dateOfBirth.message}
+            />
+          </FormControl>
+
+
           <FormControl component="fieldset" fullWidth margin="normal">
 
             <FormLabel component="legend">Gender</FormLabel>
             <Controller
               name="gender"
               control={control}
-              defaultValue=""
+              defaultValue="male"
               rules={{ required: 'Gender is required' }}
               render={({ field }) => (
                 <RadioGroup {...field} row>
---
+++ src/types/form.ts
@@ -0,0 +1,8 @@
+interface form {
+    name: string;
+    email: string;
+    password: string;
+    age: number;
+    dateOfBirth: Date;
+    gender:  'male' | 'female'| 'other';
+}
